1e18f868d51306f6eb121abbe8751f5b64db04b6
FLPstudio003 <graphicsflp@gmail.com>
2026-01-18 19:30:23 +0100

    _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
-// pages/_app.tsx
 import "@/styles/fonts.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
 import CookieConsent from "@/components/CookieConsent";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/* FIXNÉ POZADIE */}
+      <div
+        className="fixed inset-0 -z-10 bg-center bg-cover"
+        style={{
+          backgroundImage: "url('/images/bg_n.png')",
+        }}
+      />
+
+      {/* OBSAH */}
       <Component {...pageProps} />
       <CookieConsent />
     </>
