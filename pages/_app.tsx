ae2ae0a09da8482199def6681409e872f2d0186d
FLPstudio003 <graphicsflp@gmail.com>
2026-01-18 19:40:18 +0100

    _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,18 @@
+// pages/_app.tsx
+
 import "@/styles/fonts.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import CookieConsent from "@/components/CookieConsent";
+import Background from "@/components/Background"; // <- Dôležité!
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* FIXNÉ POZADIE */}
-      <div
-        className="fixed inset-0 -z-10 bg-center bg-cover"
-        style={{
-          backgroundImage: "url('/images/bg_n.png')",
-        }}
-      />
-
-      {/* OBSAH */}
+      <Background /> {/* <- Pozadie je fixnuté pod všetkým */}
       <Component {...pageProps} />
       <CookieConsent />
     </>
   );
 }
+
