a75963b376520e8f3fba8216fa44a8550f9f8ee5
FLPstudio003 <graphicsflp@gmail.com>
2026-01-18 19:39:18 +0100

    Add files via upload

---
+++ components/Background.tsx
@@ -0,0 +1,14 @@
+export default function Background() {
+  return (
+    <div
+      className="
+        fixed inset-0 -z-50
+        bg-black
+        bg-[url('/images/bg_n.png')]
+        bg-cover
+        bg-center
+        bg-no-repeat
+      "
+    />
+  );
+}
