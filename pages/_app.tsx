1d6d2b1f9b2973cf25c4bdf1f9f27d857cbe0794
FLPstudio003 <graphicsflp@gmail.com>
2026-01-19 08:27:32 +0100

    _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
-// pages/_app.tsx
-
 import "@/styles/fonts.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+
+import Background from "@/components/Background";
 import CookieConsent from "@/components/CookieConsent";
-import Background from "@/components/Background"; // <- Dôležité!
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Background /> {/* <- Pozadie je fixnuté pod všetkým */}
+      <Background />
       <Component {...pageProps} />
       <CookieConsent />
     </>
   );
 }
+
 
